a40bac4ca2c4892325ca27c9dc239c6fa578261b
Muoebonam Chukwuneke <nekeagust@gmail.com>
2025-10-18 09:59:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const reset = '\x1b[0m';
 const blue = '\x1b[34m';
 
 // Start the server
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(blue,`http://localhost:${PORT}/me`, reset);
